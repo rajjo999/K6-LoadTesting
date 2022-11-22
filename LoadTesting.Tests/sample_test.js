85d27c024024825ad4661f954936a3acb96a82fb
Chirag Verma <chirag.verma@ssense.com>
2022-11-21 21:08:37 -0500

    new tests

---
+++ LoadTesting.Tests/sample_test.js
@@ -0,0 +1,14 @@
+﻿import http from 'k6/http';
+import { sleep } from 'k6';
+
+export let options = {
+    insecureSkipTLSVerify: true,
+    noConnectionReuse: false,
+    vus: 1,
+    duration: '10s'
+};
+
+export default () => {
+    http.get('https://reqres.in/api/users?page=2');
+    sleep(1);
+};
